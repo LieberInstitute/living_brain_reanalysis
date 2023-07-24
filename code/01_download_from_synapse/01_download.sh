419931dda5319f5689be3be00d1e06d35579f262
lcolladotor <lcolladotor@gmail.com>
2023-07-24 15:09:17 -0400

    Change the log file structure and output directory

--- code/01_download_from_synapse/01_download.sh
+++ code/01_download_from_synapse/01_download.sh
@@ -16,31 +16,16 @@ echo "Job name: ${JOB_NAME}"
 echo "Hostname: ${HOSTNAME}"
 echo "Task id: ${SGE_TASK_ID}"
 
-## Load the R module (absent since the JHPCE upgrade to CentOS v7)
-module load conda_R
+## Load the Synapse module
+module load synapse/2.6.0
 
 ## List current modules for reproducibility
 module list
 
-#   Performed from the transfer node, interactively, for maximal speed:
-#       jhpce-transfer01.jhsph.edu
-
-cd /dcs04/lieber/lcolladotor/spatialDLPFC_LIBD4035/spatialDLPFC
-log_path=processed-data/synapse_upload/03-download/07-download_v5.log
-out_dir=raw-data/psychENCODE/version5
-
-mkdir -p $out_dir
-
-{
-module load synapse/2.6.0
-
 synapse \
     get \
-        -q "SELECT * FROM syn49290566.3" \
-        --downloadLocation $out_dir
-
-echo "Done at $(date)."
-} > $log_path 2>&1
+        -q "SELECT * FROM syn11346063.39" \
+        --downloadLocation ../../raw-data/psychENCODE/v2023-07-14
 
 echo "**** Job ends ****"
 date
