ffa4056ab77c26642c1f7f4dd2a1de5bc3127765
lcolladotor <lcolladotor@gmail.com>
2023-07-24 14:53:06 -0400

    Add sgejobs structure

--- code/01_download_from_synapse/01_download.sh
+++ code/01_download_from_synapse/01_download.sh
@@ -1,3 +1,27 @@
+#!/bin/bash
+#$ -cwd
+#$ -l mem_free=10G,h_vmem=10G,h_fsize=100G
+#$ -N download
+#$ -o logs/download.txt
+#$ -e logs/download.txt
+#$ -m e
+
+echo "**** Job starts ****"
+date
+
+echo "**** JHPCE info ****"
+echo "User: ${USER}"
+echo "Job id: ${JOB_ID}"
+echo "Job name: ${JOB_NAME}"
+echo "Hostname: ${HOSTNAME}"
+echo "Task id: ${SGE_TASK_ID}"
+
+## Load the R module (absent since the JHPCE upgrade to CentOS v7)
+module load conda_R
+
+## List current modules for reproducibility
+module list
+
 #   Performed from the transfer node, interactively, for maximal speed:
 #       jhpce-transfer01.jhsph.edu
 
@@ -19,3 +43,9 @@ synapse \
 
 echo "Done at $(date)."
 } > $log_path 2>&1
+
+echo "**** Job ends ****"
+date
+
+## This script was made using sgejobs version 0.99.1
+## available from http://research.libd.org/sgejobs/
