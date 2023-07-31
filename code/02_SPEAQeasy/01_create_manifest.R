9363ab7f435eeaa8ea7702a528f3ccd4116fdb29
lcolladotor <lcolladotor@gmail.com>
2023-07-31 13:33:27 -0400

    Start a script for writing a manifest for SPEAQeasy.227 out of the 7938 FASTQ files are missing. The log file for the Synapse download also ended with an error message (that wasn't necessarily related to downloading files).

---
+++ code/02_SPEAQeasy/01_create_manifest.R
@@ -0,0 +1,43 @@
+library("here")
+library("sessioninfo")
+
+## Read in information from the Synapse download query
+synapse <- read.csv(here("raw-data/psychENCODE/v2023-07-14/RNAseq/SYNAPSE_TABLE_QUERY_127308191.csv"))
+## Subset to just RNAseq data
+synapse <- subset(synapse, fileFormat == "fastq")
+dim(synapse)
+# [1] 7838   46
+
+## Check that individualID and specimenID do not overlap
+table(sapply(with(synapse, split(individualID, specimenID)), function(x) length(unique(x))))
+#   1
+# 516
+
+## Check that all fastq.gz files were downloaded
+synapse$JHPCE_path <- file.path(here("raw-data/psychENCODE/v2023-07-14/RNAseq"), synapse$name)
+synapse$JHPCE_file_exists <- file.exists(synapse$JHPCE_path)
+table(synapse$JHPCE_file_exists)
+# FALSE  TRUE
+#   227  7611
+synapse_missing <- subset(synapse, !JHPCE_file_exists)
+synapse_present <- subset(synapse, JHPCE_file_exists)
+
+unique(synapse_missing$specimenID)
+#  [1] "LBPSEMA4BRAIN639" "LBPSEMA4BRAIN730" "LBPSEMA4BRAIN176" "LBPSEMA4BRAIN285"
+#  [5] "LBPSEMA4BRAIN469" "LBPSEMA4BRAIN043" "LBPSEMA4BRAIN141" "LBPSEMA4BRAIN085"
+#  [9] "LBPSEMA4BRAIN607" "LBPSEMA4BRAIN401" "LBPSEMA4BRAIN348" "LBPSEMA4BRAIN478"
+# [13] "LBPSEMA4BRAIN402" "LBPSEMA4BRAIN778" "LBPSEMA4BRAIN623"
+unique(synapse_missing$specimenID) %in% synapse_present$specimenID
+#  [1]  TRUE FALSE FALSE FALSE FALSE FALSE FALSE FALSE FALSE FALSE FALSE FALSE
+# [13] FALSE FALSE FALSE
+
+# synapse get -q "SELECT * FROM syn11346063.39 WHERE ( \"study\" HAS ( 'LBP' ) ) AND ( \"specimenID\" LIKE '%LBPSEMA4BRAIN639%' OR \"specimenID\" LIKE '%LBPSEMA4BRAIN730%' )"
+paste(paste0("\"specimenID\" LIKE '%", unique(synapse_missing$specimenID), "' OR"), collapse = "")
+# "\"specimenID\" LIKE '%LBPSEMA4BRAIN639' OR\"specimenID\" LIKE '%LBPSEMA4BRAIN730' OR\"specimenID\" LIKE '%LBPSEMA4BRAIN176' OR\"specimenID\" LIKE '%LBPSEMA4BRAIN285' OR\"specimenID\" LIKE '%LBPSEMA4BRAIN469' OR\"specimenID\" LIKE '%LBPSEMA4BRAIN043' OR\"specimenID\" LIKE '%LBPSEMA4BRAIN141' OR\"specimenID\" LIKE '%LBPSEMA4BRAIN085' OR\"specimenID\" LIKE '%LBPSEMA4BRAIN607' OR\"specimenID\" LIKE '%LBPSEMA4BRAIN401' OR\"specimenID\" LIKE '%LBPSEMA4BRAIN348' OR\"specimenID\" LIKE '%LBPSEMA4BRAIN478' OR\"specimenID\" LIKE '%LBPSEMA4BRAIN402' OR\"specimenID\" LIKE '%LBPSEMA4BRAIN778' OR\"specimenID\" LIKE '%LBPSEMA4BRAIN623' OR"
+
+## Reproducibility information
+print("Reproducibility information:")
+Sys.time()
+proc.time()
+options(width = 120)
+session_info()
