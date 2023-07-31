da493fe3cbd7081e9923bc3b0ee6e1cfce173e64
lcolladotor <lcolladotor@gmail.com>
2023-07-31 13:37:12 -0400

    Update download script for the missing samples

--- code/01_download_from_synapse/02_download_missing.sh
+++ code/01_download_from_synapse/02_download_missing.sh
@@ -2,9 +2,9 @@
 #$ -cwd
 #$ -l rnet,mem_free=10G,h_vmem=10G,h_fsize=300G
 #$ -pe local 4
-#$ -N download
-#$ -o logs/download.txt
-#$ -e logs/download.txt
+#$ -N download_missing
+#$ -o logs/download_missing.txt
+#$ -e logs/download_missing.txt
 #$ -m e
 
 echo "**** Job starts ****"
@@ -23,19 +23,13 @@ module load synapse/2.6.0
 ## List current modules for reproducibility
 module list
 
-## Study metadata files
-synapse \
-    get \
-        -q "SELECT * FROM syn11346063.39 WHERE ( \`resourceType\` = 'metadata' ) AND ( ( \"study\" HAS ( 'LBP' ) ) )" \
-        --downloadLocation ../../raw-data/psychENCODE/v2023-07-14/metadata
-
 ## RNAseq FASTQ files
 ## Note: this includes LBP_individual_metadata.csv and
 ## LBP_biospeciment_metadata.csv which are duplicated with the metadata files.
 synapse \
     get \
-        -q "SELECT * FROM syn11346063.39 WHERE ( \"study\" HAS ( 'LBP' ) )" \
-        --downloadLocation ../../raw-data/psychENCODE/v2023-07-14/RNAseq
+        -q "SELECT * FROM syn11346063.39 WHERE ( \"study\" HAS ( 'LBP' ) ) AND ( \"specimenID\" LIKE '%LBPSEMA4BRAIN639' OR \"specimenID\" LIKE '%LBPSEMA4BRAIN730' OR \"specimenID\" LIKE '%LBPSEMA4BRAIN176' OR \"specimenID\" LIKE '%LBPSEMA4BRAIN285' OR \"specimenID\" LIKE '%LBPSEMA4BRAIN469' OR \"specimenID\" LIKE '%LBPSEMA4BRAIN043' OR \"specimenID\" LIKE '%LBPSEMA4BRAIN141' OR \"specimenID\" LIKE '%LBPSEMA4BRAIN085' OR \"specimenID\" LIKE '%LBPSEMA4BRAIN607' OR \"specimenID\" LIKE '%LBPSEMA4BRAIN401' OR \"specimenID\" LIKE '%LBPSEMA4BRAIN348' OR \"specimenID\" LIKE '%LBPSEMA4BRAIN478' OR \"specimenID\" LIKE '%LBPSEMA4BRAIN402' OR \"specimenID\" LIKE '%LBPSEMA4BRAIN778' OR \"specimenID\" LIKE '%LBPSEMA4BRAIN623' )" \
+        --downloadLocation ../../raw-data/psychENCODE/v2023-07-31/RNAseq
 
 echo "**** Job ends ****"
 date
